cde9f295383e5d6c26ff1c8f89a47fa49b9cfcfe
Satria Suryanegara <satriasuryatria@gmail.com>
2018-09-25 18:43:18 +0700

    Praktikum 2

---
+++ Latihan/app/src/main/java/sharaf/app/latihan/DetailActivity.java
@@ -0,0 +1,21 @@
+package sharaf.app.latihan;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private TextView txtData;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        txtData = (TextView) findViewById(R.id.txt_data);
+
+        String dataku = getIntent().getStringExtra("dataku");
+        txtData.setText(dataku);
+    }
+}
